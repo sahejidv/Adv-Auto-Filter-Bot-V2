6eda2465564db0242c16f455a79b1d13dd7f57be
sahejidv <89926641+sahejidv@users.noreply.github.com>
2021-09-02 00:01:09 +0900

    Add files via upload

---
+++ bot/translation.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @viha_is_power
+
+class Translation(object):
+    
+    START_TEXT = """<b>හලො මචන්  {}!!</b>
+<i>මන් විහගයගෙ බොට් බන් තොගෙ නෙවෙ. තොට ඔන්නම් එකෙක් හදා ගනින් පරයෝ ....😉
+
+Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+
+For More Details Click Help Button Below..
+@https://t.me/joinchat/Q1uroGQ645U1OTg1
+</i>"""    
+    
+    HELP_TEXT = """
+<b><i><u>මාව පවිචිචි කරන හැටිද ඔනි ,ඉන්ගිරිසියෙන් ටියෙන්නේ ඔනි එකක් කgර අනින් පරයෝ  !?</u></i></b>
+
+<i>
+-> Add Me To Any Group And Make Me Admin
+-> Add Me To Your Desired Channel
+</i>
+
+<b>Bot Commands (Works Only In Groups) :</b>
+
+    -> <code>/add chat_id</code>
+                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
+     <code>/add @Username</code>
+     
+    -> <code>/del chat_id</code>
+                OR                  - To disconnect A Group With A Channel
+     <code>/del @Username</code>
+     
+    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+    
+    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+
+            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+            
+            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+
+            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+            
+            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
+            
+@https://t.me/joinchat/Q1uroGQ645U1OTg1
+"""
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    
+<b>➥ Creator</b> : <b><i><a href="@viha_is_power">vihanga</a></i></b>
+
+<b>➥ Language</b> : <code>Python3</code>
+
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+<b>➥ Source Code</b> : <i><a href="@viha_is_power">තොපි  හිතුවඩ මට පිස්සු කියල , එනව මෙතන මගුලක් බලන්න .</a></i>
+"""
